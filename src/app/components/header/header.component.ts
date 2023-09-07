933a440d021fd703525b7c8238a3d1d6c889dd1a
Starck François <francois.starck@mil.be>
2023-09-07 17:05:32 +0200

    Added first component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-header",
+  template: ` <p>header works!</p> `,
+})
+export class HeaderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
