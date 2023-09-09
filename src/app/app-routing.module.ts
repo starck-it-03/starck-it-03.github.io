c6178e039d88357789a7cf464e778e9bcd9b3e0c
starck-it-03 <starck.it.03@gmail.com>
2023-09-09 23:54:45 +0200

    Added basic HomeComponent, routing and default

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./pages/home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    //Avec le path home,
+    path: "home",
+    //charger le componsant HomeComponent
+    component: HomeComponent,
+  },
+  {
+    // Si chemin vide, alors redirige vers page home
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full",
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
