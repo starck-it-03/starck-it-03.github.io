d84a8bb2616e699e6844e0da07573d3ea925b93e
starck-it-03 <starck.it.03@gmail.com>
2023-09-10 14:02:49 +0200

    Customed sort btn + its needed var and update fn

--- src/app/components/products-header/products-header.component.ts
+++ src/app/components/products-header/products-header.component.ts
@@ -2,17 +2,27 @@ import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-products-header",
-  //0.Create the new component (ng g componentName)
-  //1.Add a new file named 'componentName.component.html
-  //2.In it change 'template' to 'templateUrl'
-  //3.Change its value to 'componentName.component.html'
-  //4.Copy prop selector value, paste it to the appropriate component
-  //(here, as example, into 'home.component.html')
+  // 0.Create the new component (ng g componentName)
+  // 1.Add a new file named 'componentName.component.html
+  // 2.In it change 'template' to 'templateUrl'
+  // 3.Change its value to 'componentName.component.html'
+  // 4.Copy prop selector value, paste it to the appropriate component
+  // (here, as example, into 'home.component.html')
   templateUrl: "products-header.component.html",
   styles: [],
 })
 export class ProductsHeaderComponent implements OnInit {
+  // A.3
+  // Déclaration de la var pr le menu sorting et initiation
+  sort = "desc";
   constructor() {}
 
   ngOnInit(): void {}
+
+  // A.4
+  // Déclaration de la fn pr changer la valeur de variable 'sort'
+  onSortUpdated(newSort: string): void {
+    this.sort = newSort;
+    console.log(newSort);
+  }
 }
