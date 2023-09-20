26ab7883aaeae8dbfb0686c6c4b04ecc84ef3730
starck-it-03 <starck.it.03@gmail.com>
2023-09-20 14:38:33 +0100

    Added filters cpnt events + fn + var

--- src/app/components/filters/filters.component.ts
+++ src/app/components/filters/filters.component.ts
@@ -1,12 +1,17 @@
 // E.1
 // Déclaration d'un nouveau component (pour les filtres)
-import { Component, OnInit } from "@angular/core";
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 
 @Component({
   selector: "app-filters",
   templateUrl: "filters.component.html",
 })
 export class FiltersComponent implements OnInit {
+  // F.2
+  // Déclaration d'un output 'showCategory'
+  // de type EventEmitter attendant un string
+  @Output() showCategory = new EventEmitter<string>();
+
   // E.2
   // Déclaration d'un array pr les valeurs des filtres
   categories = ["Nespresso", "Senseo", "Dolce"];
@@ -14,4 +19,15 @@ export class FiltersComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  // F.1
+  // Fn pr l'émission d'un event vers le parent via output
+  onShowCategory(category: string): void {
+    // F.3
+    // Logique à éxecuter lors de l'appel de la fn 'onShowCategory'
+    this.showCategory.emit(category);
+    console.log(
+      "Print from filters.component.ts (before output): " + category
+    );
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,18 +6,29 @@ import { Component, OnInit } from "@angular/core";
   templateUrl: "./home.component.html",
 })
 export class HomeComponent implements OnInit {
-  // D.1 
+  // D.1
   // Déclaration de la var 'cols' dans le composant principale et initialisation à 3
   cols = 3;
+
+  // F.7
+  // Déclaration de la var 'category' dans le cpnt principal et non-initialisation (=> undefined obligatoire)
+  category: string | undefined;
 
   constructor() {}
 
   ngOnInit(): void {}
 
   // D.3
-  // Déclaration de la fn 'onColumnsCountChange' pr MaJ de la var 'cols' 
+  // Déclaration de la fn 'onColumnsCountChange' pr MaJ de la var 'cols'
   onColumnsCountChange(colsNum: number): void {
     this.cols = colsNum;
-    console.log("Print from home-component (post output): " + this.cols);
+    console.log("Print from home.component.ts (post output): " + this.cols);
+  }
+
+  // F.6
+  // Déclaration de la fn 'onShowCategory' pr MaJ de la var 'category'
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
+    console.log("Print from home.component.ts (post output): " + this.category);
   }
 }
