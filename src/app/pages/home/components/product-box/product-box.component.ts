e982efc78311f905f9b44fedd28ae6e220f67da1
starck-it-03 <starck.it.03@gmail.com>
2023-09-21 16:53:35 +0100

    Added dynamic classes under specific conditions

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,12 +1,17 @@
 // G.2
 // Création d'un nouveau composant spécifique aux fiches produit
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-product-box",
   templateUrl: "./product-box.component.html",
 })
 export class ProductBoxComponent implements OnInit {
+  // G.7
+  // Déclaration d'un input pr savoir si l'on est en mode coln display = 1
+  // Initialisation à false puisque page se charge en 3 coln par row
+  @Input () fullWidthMode = false;
+  
   constructor() {}
 
   ngOnInit(): void {}
