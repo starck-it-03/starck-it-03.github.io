b0e474b026dc39a359f2d20e1be7476c43c89432
starck-it-03 <starck.it.03@gmail.com>
2023-09-25 17:06:45 +0100

    Added dynamic cart page + needed function

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -11,10 +11,24 @@ export class CartComponent implements OnInit {
     items: [
       {
         product: "https://via.placeholder.com/150",
-        name: "snickers",
-        price: 150,
+        name: "Sublime 500G",
+        price: 7.79,
         quantity: 1,
         id: 1,
+      },
+      {
+        product: "https://via.placeholder.com/150",
+        name: "Chiapas 250G",
+        price: 4.99,
+        quantity: 1,
+        id: 2,
+      },
+      {
+        product: "https://via.placeholder.com/150",
+        name: "Magnifico 250G",
+        price: 4.49,
+        quantity: 1,
+        id: 3,
       },
     ],
   };
@@ -36,5 +50,12 @@ export class CartComponent implements OnInit {
   //Customed initialisation
   ngOnInit(): void {
     this.dataSource = this.cart.items;
+  }
+
+  //Fonction pour calculer le total du panier
+  getTotal(items: Array<CartItem>): number {
+    return items
+      .map((item) => item.price * item.quantity)
+      .reduce((prev, current) => prev + current, 0);
   }
 }
